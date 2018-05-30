60b9d971b11d431a1ad546d8b86fb948247b98ab
DrZepam <guendeliomar@gmail.com>
2018-05-30 19:29:24 +0100

    Dialogue test

---
+++ Assets/Dialogue System/Scripts/Supplemental/Timeline/Playables/PlaySequence/PlaySequenceBehaviour.cs
@@ -0,0 +1,22 @@
+#if UNITY_2017_1_OR_NEWER && !(UNITY_2017_3 && UNITY_WSA)
+using UnityEngine;
+using UnityEngine.Playables;
+using System;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    [Serializable]
+    public class PlaySequenceBehaviour : PlayableBehaviour
+    {
+
+        [Tooltip("Play this sequence.")]
+        [TextArea(5, 5)]
+        public string sequence;
+
+        [Tooltip("(Optional) The other subject in the sequence.")]
+        public Transform listener;
+
+    }
+}
+#endif
---
+++ Assets/Dialogue System/Scripts/Supplemental/Timeline/Playables/PlaySequence/PlaySequenceTrack.cs
@@ -0,0 +1,20 @@
+#if UNITY_2017_1_OR_NEWER && !(UNITY_2017_3 && UNITY_WSA)
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    [TrackColor(0.855f, 0.8623f, 0.87f)]
+    [TrackClipType(typeof(PlaySequenceClip))]
+    [TrackBindingType(typeof(GameObject))]
+    public class PlaySequenceTrack : TrackAsset
+    {
+        public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+        {
+            return ScriptPlayable<PlaySequenceMixerBehaviour>.Create(graph, inputCount);
+        }
+    }
+}
+#endif
