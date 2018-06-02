e3272afd04b303e11e173db613585d63f1e7963f
DrZepam <guendeliomar@gmail.com>
2018-06-02 18:11:05 +0000

    Vendor System / Dialogue wrapper implementation

---
+++ Assets/FarmMVP/Scripts/Modules/VendorDialogueWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Devdog.InventoryPro;
+using PixelCrushers.DialogueSystem;
+using UnityEngine.Events;
+public class VendorDialogueWrapper : MonoBehaviour {
+
+    public UnityEvent onTrade;
+
+    public void OnConversationLine(Subtitle subtitle)
+    {
+        Debug.Log(string.Format("{0}: {1}", subtitle.speakerInfo.transform.name, subtitle.formattedText.text));
+        if (subtitle.formattedText.text.Equals("Trade"))
+        {
+            if(onTrade != null)
+            {
+                onTrade.Invoke();            }
+        }
+    }
+
+    public void ShowVendorUI()
+    {
+        Devdog.General.Player player = GameObject.FindGameObjectWithTag("Player").GetComponent<Devdog.General.Player>();
+        VendorTrigger trigger = GetComponent<VendorTrigger>();
+        trigger.OnTriggerUsed(player);
+    }
+}
